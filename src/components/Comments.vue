7a8df82538c252d3948a8aef859dc1f11d349e8b
Anselmo Neto <anselmo.otero@gmail.com>
2020-05-15 15:47:44 -0300

    Commit inicial

---
+++ src/components/Comments.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <h1>Comentários</h1>
+        <hr />
+        <FormComment v-on:add-comment="addComment"></FormComment>
+        <div class="list-group">
+        <p v-if="comments.length <= 0">Sem comentários...</p>
+        <div v-else class="list-group-item" v-for="(comment, index) in allComments" v-bind:key="index">
+            <span class="comment__author">Autor: <strong>{{ comment.name }}</strong></span>
+            <p>{{ comment.message }}</p>
+            <div>
+            <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)">Excluir</a>
+            </div>
+        </div>
+        </div>
+        <hr />
+    </div>
+</template>
+
+<script>
+    import FormComment from './FormComment';
+
+    export default {
+        components: {
+            FormComment
+        },
+        data() {
+            return {
+            comments: []
+            }
+        },
+        methods: {
+            addComment(comment) {
+                this.comments.push(comment);
+            },
+            removeComment(index) {
+                this.comments.splice(index, 1);
+            }
+        },
+        computed: {
+            allComments() {
+            return this.comments.map(comment => ({
+                ...comment,
+                name: comment.name.trim() === '' ? 'Anônimo' : comment.name
+            }))
+            }
+        },
+        watch: {
+            comments(val) {
+            console.log('val', val)
+            }
+        }
+    }
+</script>
